78e6d978883b8173f28a6b2d23809ad0eacf7513
Jorge <jorge.sv.primero@gmail.com>
2023-12-29 17:42:26 -0300

    unit tests

--- src/domains/auth/dtos/RegisterRequestDTO.ts
+++ src/domains/auth/dtos/RegisterRequestDTO.ts
@@ -8,4 +8,9 @@ export class RegisterRequestDTO {
     message: 'password too weak',
   })
   password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
 }
