33ecbe1ea0b5409fb915028c6c1a3f146b7c8abd
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-02-26 17:07:55 -0300

    fix: added correctValue and questionnaire dtos

---
+++ src/domains/api/questionnaire/dtos/questionnaire-answer.dto.ts
@@ -0,0 +1,23 @@
+import { BubbleDto } from '../../pill/interfaces/bubble';
+
+export enum QuestionnaireState {
+  INPROGRESS = 'InProgress',
+  FAILED = 'Failed',
+  COMPLETED = 'Completed',
+}
+
+export class QuestionnaireAnswerDto {
+  questionnaireState: QuestionnaireState;
+  isCorrect: boolean;
+  progress: number;
+  pointsAwarded: number;
+  bubbles: BubbleDto[];
+
+  constructor(data: any) {
+    this.questionnaireState = data.state;
+    this.isCorrect = data.isCorrect;
+    this.progress = data.progress;
+    this.pointsAwarded = data.pointsAwarded;
+    this.bubbles = data.bubbles;
+  }
+}
