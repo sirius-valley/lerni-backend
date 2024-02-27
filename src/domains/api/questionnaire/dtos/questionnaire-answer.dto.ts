a937e9b865d15295dcd9951e2217402f6feca5dc
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-02-27 15:21:15 -0300

    feat: added correct values

--- src/domains/api/questionnaire/dtos/questionnaire-answer.dto.ts
+++ src/domains/api/questionnaire/dtos/questionnaire-answer.dto.ts
@@ -10,13 +10,15 @@ export class QuestionnaireAnswerDto {
   questionnaireState: QuestionnaireState;
   isCorrect: boolean;
   progress: number;
+  correctValues: string[];
   pointsAwarded: number;
   bubbles: BubbleDto[];
 
-  constructor(data: any) {
+  constructor(data: any, correctValues: string[]) {
     this.questionnaireState = data.state;
     this.isCorrect = data.isCorrect;
     this.progress = data.progress;
+    this.correctValues = correctValues;
     this.pointsAwarded = data.pointsAwarded;
     this.bubbles = data.bubbles;
   }
