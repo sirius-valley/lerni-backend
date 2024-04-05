4affa4185e0aff1dfca4dc6b1a2ed26741488643
Martin Ojeda <maroj074@gmail.com>
2024-04-05 09:55:01 -0300

    fix: change calulatedProgress

--- src/domains/api/achievement/achievement.service.ts
+++ src/domains/api/achievement/achievement.service.ts
@@ -10,13 +10,13 @@ export class NotificationService {
     if (!achievements) throw new HttpException('achievement not found', HttpStatus.NOT_FOUND);
     for (const achievement of achievements) {
       //add method tu calculated value
-      const progres = this.calculateProgress(achievement, 1);
       const studentAchievement = await this.achievementRepository.getStudentAchievement(student.id, achievement.id);
       if (!studentAchievement) {
         //add value
         await this.achievementRepository.createStudenAchievementLevel(student.id, achievement.id, 1);
       } else {
-        await this.achievementRepository.updateProgress(studentAchievement.id, progres);
+        //TODO: add value
+        await this.achievementRepository.updateProgress(studentAchievement.id, 1);
       }
     }
   }
