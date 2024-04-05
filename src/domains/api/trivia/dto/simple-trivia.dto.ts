0236e5a416c8ee7af9257762018dd5cab20e5953
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-04-05 11:11:55 -0300

    fix: added trivia match id to assign

--- src/domains/api/trivia/dto/simple-trivia.dto.ts
+++ src/domains/api/trivia/dto/simple-trivia.dto.ts
@@ -3,11 +3,13 @@ import { SimpleStudentDto } from '../../student/dtos/simple-student.dto';
 
 export class SimpleTriviaDto {
   triviaId: string;
+  triviaMatchId: string;
   program: SimpleProgramDto;
   opponent?: SimpleStudentDto;
 
-  constructor(triviaId: string, program: SimpleProgramDto, opponent?: SimpleStudentDto) {
+  constructor(triviaId: string, triviaMatchId: string, program: SimpleProgramDto, opponent?: SimpleStudentDto) {
     this.triviaId = triviaId;
+    this.triviaMatchId = triviaMatchId;
     this.program = program;
     this.opponent = opponent;
   }
--- src/domains/api/trivia/trivia.service.ts
+++ src/domains/api/trivia/trivia.service.ts
@@ -51,6 +51,7 @@ export class TriviaService {
       await this.assignMatchToStudent(student.id, triviaMatch.id);
       return new SimpleTriviaDto(
         triviaMatch.triviaId,
+        triviaMatch.id,
         new SimpleProgramDto(programVersion.program, 100),
         triviaMatch.studentTriviaMatches[0].student,
       );
@@ -65,6 +66,7 @@ export class TriviaService {
 
     return new SimpleTriviaDto(
       createdMatch.triviaId,
+      createdMatch.id,
       new SimpleProgramDto(programVersion.program, 100),
       opponent ? new SimpleStudentDto(opponent) : undefined,
     );
